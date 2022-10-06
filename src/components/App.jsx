ac71e0e7f5dc552dc25cb559e747054e77fec776
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-10-06 20:01:51 +0300

    done hw 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,51 @@
-export const App = () => {
-  return <div>goit-react-hw-02-feedback</div>;
-};
+import FeedbackOptions from './Feedback/FeedbackOptions';
+import Notification from './Feedback/Notification';
+import Section from './Feedback/Section';
+import Statistics from './Feedback/Statistics';
+import s from './Feedback/Feedback.module.css';
+
+const { Component } = require('react');
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = ({ target: { name } }) => {
+    this.setState({ ...this.state, [name]: this.state[name] + 1 });
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    const positivePercentage = (good * 100) / total;
+    return (
+      <div className={s.block}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleClick}
+          />
+        </Section>
+        {total > 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={Math.round(positivePercentage)}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
